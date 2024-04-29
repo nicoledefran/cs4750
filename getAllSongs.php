2124aff9b9928f3ca4976b1cde393a5265d7bf25
kaitlynrosewee <86608426+kaitlynrosewee@users.noreply.github.com>
2024-04-29 01:09:26 -0400

    Added comment on getAllSongs.php

--- getAllSongs.php
+++ getAllSongs.php
@@ -5,6 +5,7 @@ $songs = getAllSongs();
 
 // include this PHP script within a page that displays songs
 // we can loop in HTML to list all songs
+// do the same for getAllreviews();
 foreach ($songs as $song) {
     echo "<div>";
     echo "<h4>" . htmlspecialchars($song['songTitle']) . "</h4>";
